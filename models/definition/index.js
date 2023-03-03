7d838accf9372661accc8145340f1147237740eb
Attique Anwar <meattique786@gmail.com>
2023-03-03 11:50:09 +0500

    Implemented relations

--- models/definition/index.js
+++ models/definition/index.js
@@ -7,12 +7,26 @@ const User  = require('../definition/User');
 const Student  = require('../definition/Student');
 const Teacher  = require('../definition/Teacher');
 const Course  = require('../definition/Course');
-const StudentTeacher  = require('../definition/StudentTeacher');
-const StudentCourse  = require('../definition/StudentCourse');
-const TeacherCourse  = require('../definition/TeacherCourse');
+
 const models = {
-    User, Student, Teacher, Course, StudentTeacher, StudentCourse, TeacherCourse
+    User, Student, Teacher, Course,
 };
+
+
+// roles-users one to many
+User.hasMany(Student, {onDelete: 'CASCADE', foreignKey: 'userId'});
+Student.belongsTo(User, {onDelete: 'CASCADE', foreignKey: 'userId'});
+// roles-users one to many
+User.hasMany(Teacher, {onDelete: 'CASCADE', foreignKey: 'userId'});
+Teacher.belongsTo(User, {onDelete: 'CASCADE', foreignKey: 'userId'});
+
+// roles-users many to many
+Teacher.belongsToMany(Course, {through:"junction"});
+Course.belongsTo(Teacher, {through: "junction"});
+
+let junction = sequelize.models.junction;
+Student.hasMany(junction, {foreignKey: 'studentId'});
+junction.belongsTo(Student, {foreignKey: 'studentId'});
 
 
 sequelize.model = models;
